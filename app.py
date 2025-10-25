4f8a83460ddca7f1b44f29a811b3289927cf684f
Nathan10101010 <nathan.j.melia@gmail.com>
2025-10-25 20:34:17 +0000

    SPACESHIP

---
+++ app.py
@@ -0,0 +1,10 @@
+from backend import create_app
+from flask import Flask
+app = create_app()
+
+@app.route("/")
+def helloWorld():
+  return "Hello, World!"
+
+if __name__ == '__main__':
+    app.run()
--- backend/routes.py
+++ backend/routes.py
@@ -1,11 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, Blueprint, render_template
+
+routes = Blueprint('routes', __name__)
 
 app = Flask(__name__)
+
 
 # Route for your homepage, will serve your frontend HTML
 @app.route('/')
 def home():
+    print('hi')
     return render_template('index.html')  # Serve the index.html file from the templates folder
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
