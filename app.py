e8bf98188cc6e6c2089ca62afb9df0e02e4ed52c
Nathan10101010 <nathan.j.melia@gmail.com>
2025-10-25 20:56:51 +0000

    yes

--- app.py
+++ app.py
@@ -2,9 +2,5 @@ from backend import create_app
 from flask import Flask, render_template
 app = create_app()
 
-@app.route("/")
-def helloWorld():
-  return "Hell bnhjjhhjhjo, World!"
-
 if __name__ == '__main__':
     app.run()
